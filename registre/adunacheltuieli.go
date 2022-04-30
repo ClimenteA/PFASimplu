86ac125b5db522f852498b1e7827c6c2ec8ce2eb
ClimenteA <climente.alin@gmail.com>
2022-04-30 15:17:44 +0300

    more details on cheltuieli mijloc fix

--- registre/adunacheltuieli.go
+++ registre/adunacheltuieli.go
@@ -81,7 +81,26 @@ func getExpensesDataSlice(cheltuieliMetadataJson []string, anul string) []cheltu
 					SumaCheltuita:  damf.SumaCheltuita,
 					TipTranzactie:  damf.TipTranzactie,
 					Data:           damf.Data,
+					MijlocFix:      true,
 					CaleCheltuiala: damf.CaleCheltuiala,
+					DetaliiMijlocFix: cheltuieli.DetaliiMijlocFix{
+						AniAmortizare:              expense.DetaliiMijlocFix.AniAmortizare,
+						DataPuneriiInFunctiune:     expense.DetaliiMijlocFix.DataPuneriiInFunctiune,
+						CodClasificare:             expense.DetaliiMijlocFix.CodClasificare,
+						NrInventar:                 expense.DetaliiMijlocFix.NrInventar,
+						FelSerieNumarDataDocument:  expense.DetaliiMijlocFix.FelSerieNumarDataDocument,
+						ValoareInventar:            expense.DetaliiMijlocFix.ValoareInventar,
+						AmortizareLunara:           expense.DetaliiMijlocFix.AmortizareLunara,
+						DenumireSiCaracteristici:   expense.DetaliiMijlocFix.DenumireSiCaracteristici,
+						Accesorii:                  expense.DetaliiMijlocFix.Accesorii,
+						Grupa:                      expense.DetaliiMijlocFix.Grupa,
+						AnulDariiInFolosinta:       expense.DetaliiMijlocFix.AnulDariiInFolosinta,
+						LunaDariiInFolosinta:       expense.DetaliiMijlocFix.LunaDariiInFolosinta,
+						AnulAmortizariiComplete:    expense.DetaliiMijlocFix.AnulAmortizariiComplete,
+						LunaAmortizariiComplete:    expense.DetaliiMijlocFix.LunaAmortizariiComplete,
+						DurataNormalaDeFunctionare: expense.DetaliiMijlocFix.DurataNormalaDeFunctionare,
+						CotaDeAmortizare:           expense.DetaliiMijlocFix.CotaDeAmortizare,
+					},
 				}
 
 				iterChDate, _ := time.Parse(time.RFC3339, ch.Data+"T00:00:00Z")
