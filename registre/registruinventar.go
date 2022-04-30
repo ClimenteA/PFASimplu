60daad85cca4f011307ec6755b586947c7e750a4
ClimenteA <climente.alin@gmail.com>
2022-04-30 15:38:11 +0300

    arata correct mijloace fixe

--- registre/registruinventar.go
+++ registre/registruinventar.go
@@ -1,24 +1,53 @@
 package registre
 
 import (
+	"path/filepath"
+
 	"github.com/ClimenteA/pfasimplu-go/cheltuieli"
 	"github.com/ClimenteA/pfasimplu-go/types"
+	"github.com/ClimenteA/pfasimplu-go/utils"
 )
 
 func CreeazaRegistruInventar(cheltuieli []cheltuieli.Cheltuiala) []types.RegistruInventar {
 
+	mijloaceFixeSalvate := []string{}
 	inventar := []types.RegistruInventar{}
 	count := 1
 	for _, data := range cheltuieli {
-		if data.ObiectInventar || data.MijlocFix {
+
+		if data.ObiectInventar {
 			obiect := types.RegistruInventar{
 				NrCrt:            count,
 				DenumireaElemInv: data.NumeCheltuiala,
 				ValInvRon:        data.SumaCheltuita,
 			}
 			inventar = append(inventar, obiect)
+
 			count += 1
+
 		}
+
+		if data.MijlocFix {
+
+			metadataPath := filepath.Join(filepath.Dir(data.CaleCheltuiala), "metadata.json")
+
+			if !utils.SliceContains(mijloaceFixeSalvate, metadataPath) {
+
+				expense := getExpenseMetadata(metadataPath)
+
+				obiect := types.RegistruInventar{
+					NrCrt:            count,
+					DenumireaElemInv: expense.NumeCheltuiala,
+					ValInvRon:        expense.SumaCheltuita,
+				}
+
+				inventar = append(inventar, obiect)
+				mijloaceFixeSalvate = append(mijloaceFixeSalvate, metadataPath)
+				count += 1
+			}
+
+		}
+
 	}
 
 	return inventar
