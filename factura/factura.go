52384b3f3aa5b318ef3e1feab7488eb8afa37af8
ClimenteA <climente.alin@gmail.com>
2022-09-25 14:25:35 +0300

    factura html

--- factura/factura.go
+++ factura/factura.go
@@ -23,7 +23,7 @@ func handleInvoicePage(app fiber.App, store session.Store) {
 			return c.Redirect("/login")
 		}
 
-		return c.Render("factura", fiber.Map{}, "base")
+		return c.Render("editable_invoice", fiber.Map{})
 	})
 
 }
