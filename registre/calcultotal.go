a33fc945facdb9063f93916386ec36fff774c45e
ClimenteA <climente.alin@gmail.com>
2023-11-18 12:42:29 +0200

    actualizare calcul impozite pentru anul 2024

--- registre/calcultotal.go
+++ registre/calcultotal.go
@@ -52,19 +52,6 @@ type PlatiStat struct {
 }
 
 func CalculeazaPlatiCatreStat(venitNet float64, anul string) PlatiStat {
-	// - Calcul Venit Net: `venitNet = totalIncasari - totalCheltuieli - CAS`;
-	// - Calcul Impozit pe Venit: `impozitPeVenit = 10% din venitNet`;
-	// - Calcul CAS (Pensie):
-	// 	* `bazaDeCalculCAS = salariuMinimBrut x 12` - daca `venitNet` mai mare de 12 salarii minime brute pana in anul 2022 inclusiv;
-	// 	* `bazaDeCalculCAS = salariuMinimBrut x 12` - daca `venitNet` intre 12 si 24 salarii minime brute din anul 2023+;
-	// 	* `bazaDeCalculCAS = salariuMinimBrut x 24` - daca `venitNet` mai mare de 24 salarii minime brute din anul 2023+;
-	// 	* `CAS = 25% din bazaDeCalculCAS`;
-	// - Calcul CASS (Sanatate):
-	// 	* `bazaDeCalculCASS = salariuMinimBrut x 12` - daca `venitNet` mai mare de 12 salarii minime brute pana in anul 2022 inclusiv;
-	// 	* `bazaDeCalculCASS = salariuMinimBrut x 6` - daca `venitNet` mai mare de 6 salarii minime brute din anul 2023+;
-	// 	* `bazaDeCalculCAS = salariuMinimBrut x 12` - daca `venitNet` intre 12 si 24 salarii minime brute din anul 2023+;
-	// 	* `bazaDeCalculCAS = salariuMinimBrut x 24` - daca `venitNet` mai mare de 24 salarii minime brute din anul 2023+;
-	// 	* `CASS = 10% din bazaDeCalculCASS`;
 
 	anualVars := staticdata.LoadPFAConfig()
 
@@ -87,35 +74,70 @@ func CalculeazaPlatiCatreStat(venitNet float64, anul string) PlatiStat {
 		log.Panicln(err)
 	}
 
+	const (
+		ProcentCAS          = 25 // % Sanatate
+		ProcentCASS         = 10 // % Pensie
+		ProcentImpozitVenit = 10 // % Impozit pe venit
+	)
+
 	plafon6 := salariuMinimBrut * 6
 	plafon12 := salariuMinimBrut * 12
 	plafon24 := salariuMinimBrut * 24
+	plafon60 := salariuMinimBrut * 60
 
 	if anulCurrent <= 2022 {
 
 		if venitNet > plafon12 {
-			CAS = 25 * plafon12 / 100
-			CASS = 10 * plafon12 / 100
+			CAS = ProcentCAS * plafon12 / 100
+			CASS = ProcentCASS * plafon12 / 100
 		}
 
-	} else {
+		impozitPeVenit = ProcentImpozitVenit * (venitNet - CAS) / 100
+
+	}
+
+	if anulCurrent == 2023 {
 
 		if venitNet > plafon6 {
-			CASS = 10 * plafon6 / 100
+			CASS = ProcentCASS * plafon6 / 100
 		}
 
 		if venitNet > plafon12 && venitNet <= plafon24 {
-			CAS = 25 * plafon12 / 100
-			CASS = 10 * plafon12 / 100
+			CAS = ProcentCAS * plafon12 / 100
+			CASS = ProcentCASS * plafon12 / 100
 		}
 
 		if venitNet > plafon24 {
-			CAS = 25 * plafon24 / 100
-			CASS = 10 * plafon24 / 100
+			CAS = ProcentCAS * plafon24 / 100
+			CASS = ProcentCASS * plafon24 / 100
 		}
+
+		impozitPeVenit = ProcentImpozitVenit * (venitNet - CAS) / 100
 	}
 
-	impozitPeVenit = 10 * (venitNet - CAS) / 100
+	if anulCurrent >= 2024 {
+
+		if venitNet <= plafon6 {
+			CASS = ProcentCASS * plafon6 / 100
+		}
+
+		if venitNet > plafon12 && venitNet <= plafon24 {
+			CAS = ProcentCAS * plafon12 / 100
+			CASS = ProcentCASS * plafon12 / 100
+		}
+
+		if venitNet > plafon24 {
+			CAS = ProcentCAS * plafon24 / 100
+			CASS = ProcentCASS * plafon24 / 100
+		}
+
+		if venitNet > plafon60 {
+			CASS = ProcentCASS * plafon60 / 100
+		}
+
+		impozitPeVenit = ProcentImpozitVenit * (venitNet - CAS - CASS) / 100
+
+	}
 
 	total = CAS + CASS + impozitPeVenit
 
