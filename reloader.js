2f7f909f854e7c7737b7b7cd39b6fb0ebebc02a2
ClimenteA <climente.alin@gmail.com>
2024-07-17 10:28:42 +0300

    feat: reloader.js params

--- reloader.js
+++ reloader.js
@@ -1,40 +1,41 @@
-import { watch } from "fs";
+import { watch } from "fs"
+
 
 
 let server = Bun.serve({
-  port: 15173,
-  hostname: "127.0.0.1",
+  port: process.env.PORT || 15173,
+  hostname: process.env.HOST || "0.0.0.0",
   development: false,
   fetch(req, server) {
     if (server.upgrade(req)) {
-      return;
+      return
     }
-    return new Response("Reloader server ready");
+    return new Response("Reloader server ready")
   },
   websocket: {
     open(ws) {
-      console.log("WebSocket connection opened");
+      console.log("WebSocket connection opened")
       
       watch(
         import.meta.dir,
         { recursive: true },
         async (event, filename) => {
         if (filename) {
-            console.log(`Detected ${event} in ${filename}. Sending reload...`);
-            ws.send('reload');
-            console.log("Sent!");
+            console.log(`Detected ${event} in ${filename}. Sending reload...`)
+            ws.send('reload')
+            console.log("Sent!")
           }
         }
-      );
+      )
     },
     message(ws, message) {
-      console.log(`Received message: ${message}`);
+      console.log(`Received message: ${message}`)
     },
     close(ws, code, message) {
-      console.log(`WebSocket closed: ${code} ${message}`);
+      console.log(`WebSocket closed: ${code} ${message}`)
     },
   },
-});
+})
 
 
 console.info(`Reloader server listening on ${server.hostname} on port ${server.port}` )
