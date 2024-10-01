8e7bbaa8636a521f542409dab8025fcecfac7bae
ClimenteA <climente.alin@gmail.com>
2024-10-01 11:57:59 +0300

    feat: updated flaskwebgui version

--- gui.py
+++ gui.py
@@ -2,4 +2,4 @@ from flaskwebgui import FlaskUI
 from core.wsgi import application as app
 
 if __name__ == "__main__":
-    FlaskUI(app=app, server="django").run()
+    FlaskUI(app=app, server="django", app_mode=True).run()
